3e9bdbf55ad9fc24a97a5bfa1dd1c8039bd95e43
Rochie Cuevas <rochiecuevas@gmail.com>
2018-12-15 23:33:29 -0800

    added input field for date

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 // Print out the data
 console.log(data);
 
+
 // Define the date input field variable
 var inputDate = d3.select("#inputDate-field");
+var output = d3.select(".output");
+
+// Define the function "handleClick"
+function handleChange(event){
+    var text = d3.event.target.value;
+    console.log(text);
+
+    // Clear the previous value
+    output.text("");
+
+    // Set the output text
+    output.text(text);
+};
+
+
+
 
 // Create a variable for table
-var table = d3.select(".col-md-8").append("table");
+var table = d3.select(".table").append("table");
 table.attr("class", "table");
 
 var thead = table.append("thead"); // add the thead HTML element
@@ -29,7 +46,9 @@ col_headers.forEach(function(header){
     cell.text(header);
 });
 
-// Populate the tbody
+// Populate the tbody using the date in the output variable
+
+
 
 // Create a for loop using forEach (i.e., for each dict in data)
 data.forEach(function(dict){ 
@@ -52,3 +71,5 @@ data.forEach(function(dict){
 });
 
 
+// Attach an event to detect changes in the input field
+inputDate.on("change", handleChange);
