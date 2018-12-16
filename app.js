98fb93e88b6e3a35703488b0ba620040b6294cba
Rochie Cuevas <rochiecuevas@gmail.com>
2018-12-16 00:00:09 -0800

    added filtering capacity to the app.js

--- app.js
+++ app.js
@@ -3,73 +3,91 @@ console.log(data);
 
 
 // Define the date input field variable
-var inputDate = d3.select("#inputDate-field");
+
 var output = d3.select(".output");
+var submit = d3.select("#submit");
 
-// Define the function "handleClick"
-function handleChange(event){
-    var text = d3.event.target.value;
-    console.log(text);
+submit.on("click", function(){
+    // Prevent page refresh caused by clicking on Submit
+    d3.event.preventDefault();
+    
+    // Select the input HTML element
+    var inputDate = d3.select("#inputDate-field");
 
-    // Clear the previous value
-    output.text("");
+    // Get the value of the input element
+    var inputValue = inputDate.property("value");
+    
+    // Preview the inputValue
+    console.log(inputValue);
 
-    // Set the output text
-    output.text(text);
-};
+    // Filter the data based on the input date
+    var filteredDate = data.filter(d => d.datetime === inputValue);
 
+    // Preview the filteredDate
+    console.log(filteredDate);
 
+    // Create a variable for a table and its components
+    var table = d3.select(".table").append("table");
+    table.attr("class", "table");
 
+    var thead = table.append("thead"); // add the thead HTML element
+    var row_head = thead.append("tr") // Add a row in thead
+    var tbody = table.append("tbody"); // add the tbody HTML element
 
-// Create a variable for table
-var table = d3.select(".table").append("table");
-table.attr("class", "table");
+    // Populate the thead
+    var col_headers = Object.keys(filteredDate[0]); // output: array of column headers
+    console.log(col_headers);
 
-var thead = table.append("thead"); // add the thead HTML element
-var row_head = thead.append("tr") // Add a row in thead
-var tbody = table.append("tbody"); // add the tbody HTML element
+    // Create a for loop that iterates through the col_headers array
+    col_headers.forEach(function(header){
 
-// Populate the thead
-var col_headers = Object.keys(data[0]); // output: array of column headers
-console.log(col_headers);
+        // Preview each header
+        console.log(header);
 
-// Create a for loop that iterates through the col_headers array
-col_headers.forEach(function(header){
+        // Define the variable cell for each header
+        var cell = row_head.append("th");
 
-    // Preview each header
-    console.log(header);
-
-    // Define the variable cell for each header
-    var cell = row_head.append("th");
-
-    // Write each header into each cell
-    cell.text(header);
-});
-
-// Populate the tbody using the date in the output variable
-
-
-
-// Create a for loop using forEach (i.e., for each dict in data)
-data.forEach(function(dict){ 
-
-    // Add a row where values of each dict can go into
-    var row = tbody.append("tr");
-
-    // For each key-value pair in each dict
-    Object.entries(dict).forEach(function([key,value]){
-
-        // Preview each value
-        console.log(value);
-
-        // Add a cell in each row for each value
-        var cell = row.append("td");
-
-        // Write each value into each cell
-        cell.text(value);
+        // Write each header into each cell
+        cell.text(header);
     });
+
+    // Populate the tbody using the date in the output variable
+
+    // Create a for loop using forEach (i.e., for each dict in data)
+    filteredDate.forEach(function(dict){ 
+
+        // Add a row where values of each dict can go into
+        var row = tbody.append("tr");
+
+        // For each key-value pair in each dict
+        Object.entries(dict).forEach(function([key,value]){
+
+            // Preview each value
+            console.log(value);
+
+            // Add a cell in each row for each value
+            var cell = row.append("td");
+
+            // Write each value into each cell
+            cell.text(value);
+        });
+    });
+
+
 });
 
 
-// Attach an event to detect changes in the input field
-inputDate.on("change", handleChange);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
