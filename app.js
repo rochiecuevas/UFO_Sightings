8b12936e681796a98e623a6213a31d41fb145c6d
Rochie Cuevas <rochiecuevas@gmail.com>
2018-12-15 22:25:43 -0800

    created JS script that appends a table in index.html that prints the whole array in data.js

---
+++ app.js
@@ -0,0 +1,54 @@
+// Print out the data
+console.log(data);
+
+// Define the date input field variable
+var inputDate = d3.select("#inputDate-field");
+
+// Create a variable for table
+var table = d3.select(".col-md-8").append("table");
+table.attr("class", "table");
+
+var thead = table.append("thead"); // add the thead HTML element
+var row_head = thead.append("tr") // Add a row in thead
+var tbody = table.append("tbody"); // add the tbody HTML element
+
+// Populate the thead
+var col_headers = Object.keys(data[0]); // output: array of column headers
+console.log(col_headers);
+
+// Create a for loop that iterates through the col_headers array
+col_headers.forEach(function(header){
+
+    // Preview each header
+    console.log(header);
+
+    // Define the variable cell for each header
+    var cell = row_head.append("th");
+
+    // Write each header into each cell
+    cell.text(header);
+});
+
+// Populate the tbody
+
+// Create a for loop using forEach (i.e., for each dict in data)
+data.forEach(function(dict){ 
+
+    // Add a row where values of each dict can go into
+    var row = tbody.append("tr");
+
+    // For each key-value pair in each dict
+    Object.entries(dict).forEach(function([key,value]){
+
+        // Preview each value
+        console.log(value);
+
+        // Add a cell in each row for each value
+        var cell = row.append("td");
+
+        // Write each value into each cell
+        cell.text(value);
+    });
+});
+
+
