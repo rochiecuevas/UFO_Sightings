c254fd13ba54ff8d86011684c6719a0fd13985a3
Rochie Cuevas <rochiecuevas@gmail.com>
2018-12-17 00:56:53 -0800

    hero image; renamed object keys; reformatted the html; added hover style in table

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 // Print out the data
 console.log(data);
 
-// Define data using a more meaningful variable
-var sightings = data;
+// Rename the keys in data
+var map = {
+    datetime: "Date",
+    city: "City",
+    state: "State",
+    country: "Country",
+    comments: "Comments",
+    shape: "Shape",
+    durationMinutes: "Duration"
+}
 
+// Create a function that can be iterated through the objects in the data array
+// Resource: https://stackoverflow.com/a/43839611
+function editKey(obj){
+    var replacedItems = Object.keys(obj).map((key) =>{
+        var newKey = map[key] || key;
+        return{[newKey]:obj[key]};
+    });
+    var newTab = replacedItems.reduce((a, b) => Object.assign({}, a, b));
+    return newTab;
+}
 
-// Creating clickable options for filtering //
+// Use editKey function to edit the key names in each object in the data array
+var sightings = [];
+data.forEach(function(dat){
+    sightings.push(editKey(dat));
+});
+console.log(sightings);
+
 
 // Define the header row of the table using the keys of the first object
 var firstSighting = sightings[0];
@@ -15,6 +39,8 @@ var headers = [];
 Object.keys(firstSighting).forEach(key => 
     headers.push(key));
 console.log(headers);
+
+
 
 // Loop through data from sightings and puts them in each variable, remove duplicates
 var dataArr = [];
@@ -30,11 +56,11 @@ for (var i = 0; i < headers.length; i ++){
 }
 console.log(dataArr);
 
-// Adding each list of options in select input field
+// Adding the list of options in the select input field (used for filtering)
 var inputDate = d3.select("#date");
 var optDate = inputDate
-    .selectAll("option")
-    .data(dataArr[0]).enter()
+    .selectAll("option") // for each item in the list, an option HTML tag is created
+    .data(dataArr[0]).enter() // dataArr[0] corresponds to the Date in the data array
     .append("option")
     .text(function(date){
         return date;
@@ -42,7 +68,7 @@ var optDate = inputDate
 
 // Create the full table
 var table = d3.select(".table").append("table");
-    table.attr("class", "table");
+    table.attr("class", "table table-hover");
     table.attr("id", "table1");
 
 // Populate the thead using the headers array    
@@ -86,13 +112,14 @@ sightings.forEach(function(dict){
     });
 });
 
-// Filter the table
-function changeDate(){
-    tbody.text("");
-    var selDate = inputDate.property("value");
+// Filter the table (nested; i.e., choose the date first, then the state, then the city, then the alien ship shape)
+function changeDate(){ // will be activated once a choice is made in the select field for Date
+    tbody.text(""); // clears the complete table first before the filtered table is loaded
+    var selDate = inputDate.property("value"); // the selected date
     console.log(selDate);
 
-    var filterDate = sightings.filter(sighting => sighting.datetime === selDate);
+    // filter the sightings array to get all objects containing the selected date
+    var filterDate = sightings.filter(sighting => sighting.Date === selDate);
     console.log(filterDate);
 
     filterDate.forEach(function(dict){ 
@@ -114,7 +141,8 @@ function changeDate(){
         });
     });
 
-        var states = Array.from(new Set(filterDate.map(dict => dict.state)));
+        var states = Array.from(new Set(filterDate.map(dict => dict.State)));
+        states.unshift("All");
         console.log(states);  
 
         var inputState = d3.select("#state");
@@ -131,7 +159,7 @@ function changeDate(){
             var selState = inputState.property("value");
             console.log(selState);
         
-            var filterState = filterDate.filter(sighting => sighting.state === selState);
+            var filterState = filterDate.filter(sighting => sighting.State === selState);
             console.log(filterState);
 
             filterState.forEach(function(dict){ 
@@ -154,7 +182,8 @@ function changeDate(){
             });
 
 
-        var cities = Array.from(new Set(filterState.map(dict => dict.city)));
+        var cities = Array.from(new Set(filterState.map(dict => dict.City)));
+        cities.unshift("All");
         console.log(cities); 
 
         var inputCity = d3.select("#city");
@@ -171,7 +200,7 @@ function changeDate(){
             var selCity = inputCity.property("value");
             console.log(selCity);
     
-            var filterCity = filterState.filter(sighting => sighting.city === selCity);
+            var filterCity = filterState.filter(sighting => sighting.City === selCity);
             console.log(filterCity);
     
             filterCity.forEach(function(dict){ 
@@ -193,7 +222,8 @@ function changeDate(){
                 });
             });
 
-            var shapes = Array.from(new Set(filterCity.map(dict => dict.shape)));
+            var shapes = Array.from(new Set(filterCity.map(dict => dict.Shape)));
+            shapes.unshift("All");
             console.log(shapes);
 
             var inputShape = d3.select("#shape");
@@ -206,30 +236,31 @@ function changeDate(){
                 });
 
             function changeShape(){
-            var selShape = inputShape.property("value");
-            console.log(selShape);
+                tbody.text("");
+                var selShape = inputShape.property("value");
+                console.log(selShape);
 
-            var filterShape = filterCity.filter(sighting => sighting.shape === selShape);
-            console.log(filterShape);
+                var filterShape = filterCity.filter(sighting => sighting.Shape === selShape);
+                console.log(filterShape);
 
-            filterShape.forEach(function(dict){ 
+                filterShape.forEach(function(dict){ 
 
-                // Add a row where values of each dict can go into
-                var row = tbody.append("tr");
-            
-                // For each key-value pair in each dict
-                Object.entries(dict).forEach(function([key,value]){
-            
-                    // Preview each value
-                    console.log(value);
-            
-                    // Add a cell in each row for each value
-                    var cell = row.append("td");
-            
-                    // Write each value into each cell
-                    cell.text(value);
+                    // Add a row where values of each dict can go into
+                    var row = tbody.append("tr");
+                
+                    // For each key-value pair in each dict
+                    Object.entries(dict).forEach(function([key,value]){
+                
+                        // Preview each value
+                        console.log(value);
+                
+                        // Add a cell in each row for each value
+                        var cell = row.append("td");
+                
+                        // Write each value into each cell
+                        cell.text(value);
+                    });
                 });
-            });
 
             };
             inputShape.on("change", changeShape);
@@ -239,3 +270,12 @@ function changeDate(){
     inputState.on("change", changeState);
 };
 inputDate.on("change", changeDate);
+
+// Refresh button
+var button = d3.select(".btn");
+function refreshPage(){
+    window.location.reload(true);
+}
+
+setFilterGrid("table");
+
